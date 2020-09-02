5cd56d2ff719c180bc306053153823786185933a
akshit-im <68770976+akshit-im@users.noreply.github.com>
2020-09-03 00:34:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibo = new List<int> { 1, 1 };
+            while (fibo.Count<20)
+            {
+                var x = fibo[fibo.Count - 1];
+                var y = fibo[fibo.Count - 2];
+
+                fibo.Add(x + y);
+
+            }
+            foreach (var item in fibo)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
